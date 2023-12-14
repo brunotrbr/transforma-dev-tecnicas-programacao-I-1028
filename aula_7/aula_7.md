2aa277028539a910a400184c98db8f0805ebf2f7
Bruno Trindade Bragança <bbmendes@gmail.com>
2023-12-13 22:03:09 -0300

    aula

--- aula_7/aula_7.md
+++ aula_7/aula_7.md
@@ -34,7 +34,7 @@ Um processo é, basicamente, um programa em execução. Ele possui seu próprio 
 
 ## Threads
 
-As *threads* podem ser ententidas como processos mais leves, que possuem um espaço de memória compartilhado, cooperam entre si por tempo de CPU, compartilham também os dados e existem **dentro** de um processo.
+As *threads* podem ser entendidas como processos mais leves, que possuem um espaço de memória compartilhado, cooperam entre si por tempo de CPU, compartilham também os dados e existem **dentro** de um processo.
 
 &nbsp;
 
@@ -116,7 +116,28 @@ O .NET já possui uma biblioteca padrão para utilizar programação paralela, c
 Vamos considerar o código abaixo:
 
 ```csharp
+app.MapGet("/aula_7", () =>{
+    Console.WriteLine("Aula 7");
+    // Thread.Sleep(15000);
 
+    Console.WriteLine("Aperte enter para inicar");
+    Console.ReadLine();
+    ProcessarLaco();
+    Console.WriteLine("\n");
+
+    Parallel.For(0, 11, i => Console.WriteLine($"[Síncrono] Valor := {i}, Thread := {Thread.CurrentThread.ManagedThreadId}"));
+    Console.WriteLine("\n");
+
+    
+    return "Transforma DEV";
+});
+
+static void ProcessarLaco(){
+    for(int i = 0; i <= 10; i++){
+        Console.WriteLine($"[Parallel.For] Valor := {i} \t Thread := {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(500);
+    }
+}
 ```
 
 A função processar laço vai percorrer os números de 0 a 10 e imprimir na tela seu valor e qual thread foi utilizada.
@@ -128,7 +149,12 @@ Quando usamos o `Parallel.For`, o número é processado por alguma thread (mas n
 Podemos utilizar também o `Parallel.ForEach`, que vai funcionar de forma semelhante ao ForEach sequencial.
 
 ```csharp
-
+List<int> inteiros = new List<int>();
+for(int i = 0; i <= 10; i++){
+    inteiros.Add(i);
+}
+Parallel.ForEach(inteiros, i => Console.WriteLine($"[Parallel.ForEach] Valor := {i}, Thread := {Thread.CurrentThread.ManagedThreadId}"));
+Console.WriteLine("\n");
 ```
 
 &nbsp;
--- source/ProjetoAulas/Aula7.cs
+++ source/ProjetoAulas/Aula7.cs
@@ -1,10 +1,52 @@
+using System.Diagnostics;
+using System.Security.Cryptography;
+
 namespace ProjetoAulas
 {
     public static class Aula7
     {
         public static void MapAula7Endpoints(this WebApplication app)
         {
-            
+            app.MapGet("/aula_7", () =>{
+                Console.WriteLine("Aula 7");
+                // Thread.Sleep(15000);
+
+                Console.WriteLine("Aperte enter para inicar");
+                Console.ReadLine();
+
+                DateTime start = DateTime.UtcNow;
+                ProcessarLaco();
+                DateTime stop = DateTime.UtcNow;
+                TimeSpan ts = stop - start;
+                Console.WriteLine($"[Síncrono] {ts.TotalMilliseconds}");
+                Console.WriteLine("\n");
+                
+                start = DateTime.UtcNow;
+                Parallel.For(0, 11, i => Console.WriteLine($"[Parallel.For] Valor := {i}, Thread := {Thread.CurrentThread.ManagedThreadId}"));
+                stop = DateTime.UtcNow;
+                ts = stop - start;
+                Console.WriteLine($"[Parallel.For] {ts.TotalMilliseconds}");
+                Console.WriteLine("\n");
+
+                List<int> inteiros = new List<int>();
+                for(int i = 0; i <= 10; i++){
+                    inteiros.Add(i);
+                }
+                start = DateTime.UtcNow;
+                Parallel.ForEach(inteiros, i => Console.WriteLine($"[Parallel.ForEach] Valor := {i}, Thread := {Thread.CurrentThread.ManagedThreadId}"));
+                stop = DateTime.UtcNow;
+                ts = stop - start;
+                Console.WriteLine($"[Parallel.ForEach] {ts.TotalMilliseconds}");
+                Console.WriteLine("\n");
+                
+                return "Transforma DEV";
+            });
+
+            static void ProcessarLaco(){
+                for(int i = 0; i <= 10; i++){
+                    Console.WriteLine($"[Síncrono] Valor := {i} \t Thread := {Thread.CurrentThread.ManagedThreadId}");
+                }
+            }
         }
     }
 }
